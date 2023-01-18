8cfa833af6454269f630989c3ceab55a5c9e900f
LoganRankine <L.RANKINE_2020@hull.ac.uk>
2023-01-18 16:37:10 +0000

    page background added with title and sign in form

--- index.js
+++ index.js
@@ -4,8 +4,36 @@ const hostname = '127.0.0.1'
 const port = 3000
 const fs = require('fs').promises;
 
-const server = http.createServer((req, res) => {
-  fs.readFile(__dirname + "/index.html")
+const server = http.createServer(async (req, res) => {
+  
+  switch (req.url) {
+    case "/":
+        var contents = await fs.readFile("login.html");
+        res.end(contents);
+        break;
+    case "/style.css":
+        var contents = await fs.readFile("style.css");
+        res.end(contents);
+        break;
+    case "/index.js":
+        var contents = await fs.readFile("index.js");
+        res.end(contents);
+        break;
+    default:
+        res.writeHead(404);
+        res.end("404 not found")
+        return;
+        break;
+  }
+
+})
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
+
+/*
+  fs.readFile(__dirname + "/login.html")
   .then(contents => {
       res.setHeader("Content-Type", "text/html");
       res.writeHead(200);
@@ -16,8 +44,4 @@ const server = http.createServer((req, res) => {
       res.end(err);
       return;
   });
-})
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
-})
+*/
